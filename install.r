e1b96ff7aaa930679ca72a2d77a1858ee7686ea4
Marine Analyst <78915180+Marine-Analyst@users.noreply.github.com>
2021-02-12 12:39:55 +0100

    Update install.r

--- install.r
+++ install.r
@@ -1,10 +1,9 @@
-# install gdal
-system('sudo apt-get update')
-system('sudo apt-get install libgdal-dev libproj-dev')
-# install required R packages
-install.packages(c("rgdal","raster","mapdata","maptools","downloader","directlabels","rasterVis","XML","ncdf4","kableExtra","rjson"), repos='http://cran.rstudio.com/')
 install.packages('ggplot2')
 install.packages('ggmap')
 install.packages('ggrepel')
-install.packages('rgeos')
+install.packages('raster')
+install.packages('rasterVis')
+install.packages('mapdata')
 install.packages('sp')
+install.packages('downloader')
+install.packages('rgdal')
